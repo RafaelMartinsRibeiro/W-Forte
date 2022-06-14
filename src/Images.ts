aaee6a2139aa778a78e775a25474ff41dae9f984
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-06-14 19:51:24 -0300

    Added Object Images

---
+++ src/Images.ts
@@ -0,0 +1,35 @@
+export const Images = {
+  gymImages: [
+    { id: 1, image: "internalEquipment1.jpg" },
+    { id: 2, image: "externalEquipment2.jpg" },
+    { id: 3, image: "internalEquipment3.jpg" },
+  ],
+  internalEquipments: [
+    { id: 1, image: "internalEquipment1.jpg" },
+    { id: 2, image: "internalEquipment2.jpg" },
+    { id: 3, image: "internalEquipment3.jpg" },
+    { id: 4, image: "internalEquipment4.jpg" },
+    { id: 5, image: "internalEquipment5.jpg" },
+  ],
+  externalEquipments: [
+    { id: 1, image: "externalEquipment1.jpg" },
+    { id: 2, image: "externalEquipment2.jpg" },
+    { id: 3, image: "externalEquipment3.jpg" },
+    { id: 4, image: "externalEquipment4.jpg" },
+    { id: 5, image: "externalEquipment5.jpg" },
+  ],
+  modalities: [
+    { id: 1, image: "modality1.jpg" },
+    { id: 2, image: "modality2.jpg" },
+    { id: 3, image: "modality3.jpg" },
+    { id: 4, image: "modality4.jpg" },
+    { id: 5, image: "modality5.jpg" },
+  ],
+  professors: [
+    { id: 1, image: "professor1.jpg" },
+    { id: 2, image: "professor2.jpg" },
+    { id: 3, image: "professor3.jpg" },
+    { id: 4, image: "professor4.jpg" },
+    { id: 5, image: "professor5.jpg" },
+  ],
+};
