8571370b2e7cc17cba5827d0c592aba5a3e971f6
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-06-24 22:26:10 -0300

    Added responsive style in Professors Component

--- src/components/Estrutura/Professors.tsx
+++ src/components/Estrutura/Professors.tsx
@@ -18,7 +18,7 @@ export function Professors() {
 
   return (
     <div>
-      <h3 className="w-full bg-brand-yellow p-3 font-black text-lg text-[#000000f1] flex justify-center items-center  shadow-[#00000085]">
+      <h3 className="w-full bg-brand-yellow p-3 font-black text-sm sm:text-base md:text-lg text-[#000000f1] flex justify-center items-center  shadow-[#00000085]">
         Alguns de nossos professores
       </h3>
 
@@ -32,14 +32,14 @@ export function Professors() {
       >
         <div className=" py-7 backdrop-blur-md bg-black bg-opacity-70 w-full h-full flex flex-col items-center justify-center">
           <div className="flex flex-col justify-center items-center">
-            <h2 className="flex items-center justify-center bg-transparent text-brand-yellow w-[35rem]  my-2 p-3 border-[2px] border-brand-yellow border-solid rounded-xl font-black text-lg transition-colors duration-200">
+            <h2 className="text-sm sm:text-base md:text-lg flex items-center justify-center bg-transparent text-brand-yellow w-64 sm:w-[21rem] md:w-[26rem] lg:w-[35.1rem] my-2 p-3 border-[2px] border-brand-yellow border-solid rounded-xl font-black transition-colors duration-200">
               {Images.professors[professor - 1].name}
             </h2>
 
             <div className="flex items-center justify-center">
               <SwapLeftOutlined
                 onClick={changePreviousEquipment}
-                className="text-brand-yellow text-6xl leading-3 mr-10 rounded-full  cursor-pointer opacity-70 transition-all duration-200  shadow-lg shadow-[#000000bb] hover:opacity-100 active:shadow-inner active:shadow-[#000000bb]"
+                className="text-brand-yellow text-2xl sm:text-5xl md:text-6xl leading-3 mr-5 sm:mr-10 rounded-full  cursor-pointer opacity-70 transition-all duration-200  shadow-lg shadow-[#000000bb] hover:opacity-100 active:shadow-inner active:shadow-[#000000bb]"
               />
 
               <div
@@ -55,7 +55,7 @@ export function Professors() {
                     transition={{ duration: 0.5 }}
                     src={`/assets/${Images.professors[professor - 1].image}`}
                     alt="professor"
-                    className="w-[35.1rem] h-[30rem] shadow-md shadow-[#00000085] rounded-2xl"
+                    className="w-64 sm:w-[21rem] md:w-[26rem] lg:w-[35.1rem] h-[17rem] sm:h-[23rem] md:h-[27rem] lg:h-[30rem] shadow-md shadow-[#00000085] rounded-2xl"
                     style={{ gridArea: "content" }}
                   />
                 </AnimatePresence>
@@ -63,7 +63,7 @@ export function Professors() {
 
               <SwapRightOutlined
                 onClick={changeNextEquipment}
-                className="text-brand-yellow text-6xl leading-3 ml-10 rounded-full  cursor-pointer opacity-70 transition-all duration-200  shadow-lg shadow-[#000000bb] hover:opacity-100 active:shadow-inner active:shadow-[#000000bb]"
+                className="text-brand-yellow text-2xl sm:text-5xl md:text-6xl leading-3 ml-5 sm:ml-10 rounded-full  cursor-pointer opacity-70 transition-all duration-200  shadow-lg shadow-[#000000bb] hover:opacity-100 active:shadow-inner active:shadow-[#000000bb]"
               />
             </div>
             <div className="flex items-center justify-center gap-6 mt-5 z-20">
